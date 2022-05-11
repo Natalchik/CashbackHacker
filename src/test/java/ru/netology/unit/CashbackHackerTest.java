2f3e1963a94a2dbb8ccde667b1ebfcdfa2231653
Nataly Kalinina <annataly-1238@ya.ru>
2022-05-11 17:51:08 +0300

    Junit4 added

--- src/test/java/ru/netology/unit/CashbackHackerTest.java
+++ src/test/java/ru/netology/unit/CashbackHackerTest.java
@@ -1,34 +1,40 @@
 package ru.netology.unit;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackerTest {
 
     @Test
+
     public void shouldShowRemainderTo1000ToGetBonusIfLessThan1000() {
         CashbackHacker service = new CashbackHacker();
         int amount = 900;
-        int actual = service.remain(amount);
         int expected = 100;
-    assertEquals(actual, expected);
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldShowRemainderToTheNext1000ToGetBonusIfMoreThan1000() {
         CashbackHacker service = new CashbackHacker();
         int amount = 1100;
-        int actual = service.remain(amount);
         int expected = 900;
-        assertEquals(actual, expected);
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
     }
+
     @Test
     public void shouldNotShowRemainderWhen1000() {
         CashbackHacker service = new CashbackHacker();
         int amount = 1000;
-        int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
     }
 }
